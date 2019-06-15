90422501b7075caf594f13e96910c3d486871f7a
Ruben <rwoldhui@avans.nl>
2019-06-15 19:55:57 +0200

    [Reset] again

--- OpenGLAssesment/drawable.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class drawable
-{
-public:
-	virtual void draw() const
-	{
-	}
-};
--- OpenGLAssesment/globals.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#define APPLICATION_NAME "A Simulation"
-#define WINDOW_WIDTH 1280
-#define WINDOW_HEIGHT 720
